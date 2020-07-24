be0caacc8145f05df3ef78646c3675d283de4c69
Manuel Neumayer <manuel.neumayer@campus.lmu.de>
2020-07-24 16:31:05 +0200

    Added FontAwesome Icons, refactored code

--- src/Job.js
+++ src/Job.js
@@ -1,18 +1,27 @@
 import React, { useState } from 'react';
 import { Card, Badge, Button, Collapse } from 'react-bootstrap';
 import ReactMarkdown from 'react-markdown';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInfoCircle, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
 
 export default function Job({ job }) {
     const [open, setOpen] = useState(false);
 
-    let today = new Date(),
-    createdOn = new Date(job.created_at),
-    msInDay   = 24 * 60 * 60 * 1000;
+    /**
+     * Calculates the difference of today's time and the job posting's creation time in days.
+     * @param {*} createdAt The Date that the job posting was created at
+     */
+    function getDifferenceInDays(createdAt) {
+        let today = new Date(),
+        createdOn = new Date(createdAt),
+        msInDay   = 24 * 60 * 60 * 1000;
 
-    // Normalize the hours and calculate differenceInDays
-    today.setHours(0,0,0,0);
-    createdOn.setHours(0,0,0,0);
-    let differenceInDays = (today.getTime() - createdOn.getTime()) / msInDay
+        // Normalize the hours and calculate the difference in days
+        today.setHours(0,0,0,0);
+        createdOn.setHours(0,0,0,0);
+
+        return (today.getTime() - createdOn.getTime()) / msInDay
+    }
 
     return (
         <Card className="mb-3 box-shadow">
@@ -23,10 +32,13 @@ export default function Job({ job }) {
                             {job.title} - <span className="text-muted font-weight-light">{job.company}</span>
                         </Card.Title>
                         <Card.Subtitle className="text-muted mb-2">
-                            {new Date(job.created_at).toLocaleDateString()} - {differenceInDays} days ago
+                            {new Date(job.created_at).toLocaleDateString()} - {getDifferenceInDays(job.created_at)} days ago
                         </Card.Subtitle>
                         <Badge variant="secondary" className="mr-2">{job.type}</Badge>
-                        <Badge variant="secondary">{job.location}</Badge>
+                        <Badge variant="secondary">
+                            <FontAwesomeIcon className="mr-2" icon={faMapMarkerAlt} size="md" />
+                            {job.location}
+                        </Badge>
                         <div className="my-2" style={{ wordBreak: 'break-all' }}>
                             <ReactMarkdown source={job.how_to_apply} />
                         </div>
@@ -36,7 +48,7 @@ export default function Job({ job }) {
                 <Card.Text>
                     <Button onClick={() => setOpen(prevOpen => !prevOpen)}
                             variant="primary">
-                            {open ? 'Hide Details' : 'View Details'}
+                            <FontAwesomeIcon className="mr-2" icon={faInfoCircle} size="md" /> {open ? 'Hide Details' : 'View Details'}
                     </Button>
                 </Card.Text>
 
